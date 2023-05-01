63f4ca90ed0958217c7dc7f86ab456098f10e71c
Mario Testowy <mario.testowy@testing.xyz>
2023-05-01 13:57:33 +0200

    Revert "Modification 2"
    This reverts commit 8461b80b

--- src/main/java/pl/zajavka/case1/Dog.java
+++ src/main/java/pl/zajavka/case1/Dog.java
@@ -1,9 +1,9 @@
 package pl.zajavka.case1;
 
 public class Dog extends Animal {
-    private String name;
 
-    public void sniff() {
-        System.out.println("I'm soo sniffing!");
+    @Override
+    public String whatIsYourName() {
+        return "My name is Dog";
     }
 }

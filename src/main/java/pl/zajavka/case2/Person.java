244dfc4267d18c434dbdd11a0674717cecf370d6
Mario Testowy <mario.testowy@testing.xyz>
2023-05-02 12:25:39 +0200

    Person class modification 3

--- src/main/java/pl/zajavka/case2/Person.java
+++ src/main/java/pl/zajavka/case2/Person.java
@@ -4,11 +4,24 @@ import java.math.BigDecimal;
 
 record Person(String name, Long age, BigDecimal salary) {
 
-    public Boolean doYouHaveABike() {
-        return true;
+    public BigDecimal howMuchDoYouEarn() {
+        return salary;
+    }
+
+    public String whatIsYourJob() {
+        return "Software developer!";
+    }
+
+    public String whatDoYouLike() {
+        return "Strawberries";
     }
 
     public Boolean doYouLikePizza() {
         return true;
     }
+
+    public Boolean doYouHaveABike() {
+        return true;
+    }
+
 }

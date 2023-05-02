97cd5e3523712f6f02c711de45c51e1ff1af1af8
Mario Testowy <mario.testowy@testing.xyz>
2023-05-02 10:35:34 +0200

    Modification 3 after correction of modification 2

--- src/main/java/pl/zajavka/case1/Dog.java
+++ src/main/java/pl/zajavka/case1/Dog.java
@@ -2,9 +2,9 @@ package pl.zajavka.case1;
 
 public class Dog extends Animal {
     //  Commit 2 modification
+    private String name;
 
-    @Override
-    public String whatIsYourName() {
-        return "My name is Dog";
+    public void sniff() {
+        System.out.println("I'm soo sniffing!");
     }
 }
